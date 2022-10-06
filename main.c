0f5ec9c3bb5f843b57026775155eba5f4ad0e4a2
Carlos David Mesa Martinez <dm31284@gmail.com>
2022-10-05 19:58:29 -0500

    final main

--- main.c
+++ main.c
@@ -10,15 +10,31 @@
 int esDirectorio(char * path);
 int search(char*dir, char*pattern);
 
+/**
+ * @brief Inicializa el programa con 3 parametros 
+ * 
+ * @param argc 3 parametros para poder inicializar
+ * @param argv argv[0]->ejecutable, argv[1]->ruta de directorio, argv[2]->patron a buscar
+ * @return int 
+ */
 int main(int argc, char*argv[]){
-   	printf("Primer arg: [%s]\n",argv[1]);
-        printf("Segundo arg: [%s\n]", argv[2]);	
-	//int resp = search(argv[1], argv[2]);
-	//printf("%d", resp);	
+    if (argc !=3){
+        fprintf(stderr, "uso: %s DIR PATRON\n", argv[0]);
+        exit(EXIT_SUCCESS);
+    }
+
+	int resp = search(argv[1], argv[2]);
+	printf("TOTAL=[%d]", resp);	
 
     exit(EXIT_SUCCESS);
 }
 
+/**
+ * @brief Define si una ruta es un directorio,  0 FALSO || 1 VERDADERO
+ * 
+ * @param path ruta a determinar si es directorio
+ * @return int 0 FALSO || 1 VERDADERO
+ */
 int esDirectorio(char * path){
     struct stat sb;
     int resp = stat(path, &sb);
@@ -33,37 +49,45 @@ int esDirectorio(char * path){
     return 0;
 }
 
+/**
+ * @brief Busca de de forma recursiva dentro de un directorio todos los archivos que contengan un patron dado
+ * 
+ * @param dir ruta del directorio
+ * @param pattern patron a buscar en el nombre de los archivos dentro del directorio y su subdirectorios
+ * @return int total de archivos encontrados con el patrón
+ */
 int search(char*dir, char*pattern){
 	int total = 0;
 	DIR*streamDir = opendir(dir);
 	
+	//**Verificar que el directorio sea valido
 	if(streamDir == NULL){
 		perror("opendir");
 		return 0;
 	}
 	
 	struct dirent *entry;
-	int dirLenght = strlen(dir);
-	
 	char*absPath;
+	
+	//**Leemos los archivos hasta que encontremos un null indicando que se han leido todos
 	while( (entry = readdir(streamDir) ) ){
-		int fileLenght = strlen(entry->d_name)+1;		
-	    absPath = (char*)malloc(dirLenght+fileLenght);
+		//reservamos memoria dinamica para almacenar la ruta absoluta del archivo en lectura
+	    absPath = (char*)malloc(strln(dir) + strlen (entry->d_name)+2);
 	  
 	    strcpy(absPath, dir);
 	    strcat(absPath, "/");
 	    strcat(absPath, entry->d_name);	  
 	    
+		//verificamos si el archivo cumple el patron
 	    if(strstr(entry->d_name, pattern) ){
 	    	total += 1;
 		}
 		
-	    //printf("%s\n", absPath);	    	
+		//buscamos recursivamente en los subdirectorios
 		if(esDirectorio(dir) == 1 
 			&& strcasecmp(entry->d_name, ".") != 0
 			&& strcasecmp(entry->d_name, "..") != 0	
 		){
-	    	//printf("%s\n", absPath);	    	
 			total = total + search(absPath, pattern);
 		}	
 		
